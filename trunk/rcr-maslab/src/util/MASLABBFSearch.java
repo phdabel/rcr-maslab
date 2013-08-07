c13e8a58348bffd4636cbfbfede4efb621ac1eff
cristianogalafassi@gmail.com <cristianogalafassi@gmail.com@150ea5b6-1bd1-6676-5749-7374f3dfea70>
2013-08-07 15:30:27 +0000

    Ajustar erro... Ainda trabalhando nisso...

--- trunk/rcr-maslab/src/util/MASLABBFSearch.java
+++ trunk/rcr-maslab/src/util/MASLABBFSearch.java
@@ -4,6 +4,7 @@ import rescuecore2.worldmodel.EntityID;
 import rescuecore2.worldmodel.Entity;
 import rescuecore2.misc.collections.LazyMap;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Arrays;
@@ -12,6 +13,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collection;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import rescuecore2.standard.entities.StandardWorldModel;
 import rescuecore2.standard.entities.Area;
@@ -143,4 +145,58 @@ public final class MASLABBFSearch {
     private boolean isBlocked(EntityID e, Collection<EntityID> test) {
         return test.contains(e);
     }
+    
+    /**
+    Do a breadth first search from one location to the closest (in terms of number of nodes) of a set of goals.
+    @param start The location we start at.
+    @param goals The set of possible goals.
+    @return The path from start to one of the goals, or null if no path can be found.
+ */
+	public List<EntityID> CompleteBreadthFirstSearch(EntityID start, Collection<EntityID> goals) {
+		List<EntityID> open = new LinkedList<EntityID>();
+		Map<EntityID, EntityID> ancestors = new HashMap<EntityID, EntityID>();
+		open.add(start);
+		EntityID next = null;
+		boolean found = false;
+		ancestors.put(start, start);
+		do {
+			next = open.remove(0);
+			if (!isGoal(next, goals)) {
+				Collection<EntityID> neighbours = graph.get(next);
+				if (neighbours.isEmpty()) {
+				    continue;
+				}
+				for (EntityID neighbour : neighbours) {
+				    if (isGoal(neighbour, goals)) {
+				        ancestors.put(neighbour, next);
+				        next = neighbour;
+				        found = true;
+				        break;
+				    }
+				    else {
+				        if (!ancestors.containsKey(neighbour)) {
+				            open.add(neighbour);
+				            ancestors.put(neighbour, next);
+				        }
+				    }
+				}
+			}
+		} while (!found && !open.isEmpty());
+		if (!found) {
+		    // No path
+		    return new ArrayList<EntityID>();
+		}
+		// Walk back from goal to start
+		EntityID current = next;
+		List<EntityID> path = new LinkedList<EntityID>();
+		do {
+		    path.add(0, current);
+		    current = ancestors.get(current);
+		    if (current == null) {
+		        throw new RuntimeException("Found a node with no ancestor! Something is broken.");
+		    }
+	    } while (current != start);
+	    return path;
+	}
+
 }
