8ffdb5886e3e4b5db1b93beb0702936a01f21d28
phd.abel@gmail.com <phd.abel@gmail.com@150ea5b6-1bd1-6676-5749-7374f3dfea70>
2013-08-31 22:29:25 +0000

    terminado algoritmo de ClosestPairPoints com restrições

--- branches/abel/rcr-maslab/src/util/MASLAB/ClosestPair.java
+++ branches/abel/rcr-maslab/src/util/MASLAB/ClosestPair.java
@@ -10,10 +10,10 @@ import util.MyPoint2D;
 
 public class ClosestPair {
 
-	private Point2D best1, best2;
+	private MyPoint2D best1, best2;
 	private double bestDistance = Double.POSITIVE_INFINITY;
-	private ArrayList<Point2D> points1 = new ArrayList<Point2D>();
-	private ArrayList<Point2D> points2 = new ArrayList<Point2D>();
+	private ArrayList<MyPoint2D> points1 = new ArrayList<MyPoint2D>();
+	private ArrayList<MyPoint2D> points2 = new ArrayList<MyPoint2D>();
 	
 	public ClosestPair(Point2D[] points1, Point2D[] points2)
 	{
@@ -21,16 +21,20 @@ public class ClosestPair {
 		int N = points1.length + points2.length;
 		if(N <= 1) return;
 		
-		Point2D[] pointsByX = new Point2D[N];
+		
+		MyPoint2D[] pointsByX = new MyPoint2D[N];
 		for(int i = 0; i < points1.length; i++)
 		{
-			pointsByX[i] = points1[i];
-			this.points1.add(points1[i]);
+			pointsByX[i] = new MyPoint2D(points1[i].getX(), points1[i].getY());
+			this.points1.add(pointsByX[i]);
 		}
-		int auxN = pointsByX.length;
+		int auxN = points1.length;
 		for(int i = 0; i < points2.length; i++){
-			pointsByX[auxN + i] = points2[i];
-			this.points2.add(points2[i]);
+			System.out.println("posicao "+(auxN+i));
+			System.out.println("Ponto "+points2[i]);
+			
+			pointsByX[auxN+i] = new MyPoint2D(points2[i].getX(), points2[i].getY());
+			this.points2.add(pointsByX[auxN+i]);
 		}
 		
 		Arrays.sort(pointsByX, new Point2DXSorter());
@@ -44,27 +48,28 @@ public class ClosestPair {
 			}
 		}
 		
-		Point2D[] pointsByY = new Point2D[N];
+		MyPoint2D[] pointsByY = new MyPoint2D[N];
 		for(int i = 0; i < N; i++) pointsByY[i] = pointsByX[i];
 		
-		Point2D[] aux = new Point2D[N];
+		MyPoint2D[] aux = new MyPoint2D[N];
 		
-		closest(pointsByX, pointsByY, aux, 0, N-1);
+		closest(pointsByX, pointsByY, aux, 0, (N-1));
 		
 	}
 	
-	public double closest(Point2D[] pointsByX, Point2D[] pointsByY, Point2D[] aux, int lo, int hi)
+	public double closest(MyPoint2D[] pointsByX, MyPoint2D[] pointsByY, MyPoint2D[] aux, int lo, int hi)
 	{
 		if(hi <= lo) return Double.POSITIVE_INFINITY;
 		int mid = lo + (hi - lo) / 2;
 		
-		Point2D median = pointsByX[mid];
+		MyPoint2D median = pointsByX[mid];
 		
 		double delta1 = closest(pointsByX, pointsByY, aux, lo, mid);
 		double delta2 = closest(pointsByX, pointsByY, aux, mid+1, hi);
 		double delta = Math.min(delta1, delta2);
 		
-		Merge.merge((MyPoint2D[])pointsByY, (MyPoint2D[])aux, lo, mid, hi);
+		
+		Merge.merge(pointsByY, aux, lo, mid, hi);
 		
 		int M = 0;
 		
@@ -80,14 +85,16 @@ public class ClosestPair {
 		{
 			for(int j = i+1; (j<M) && (aux[j].getY() - aux[i].getY() < delta); j++)
 			{
-				double distance = GeometryTools2D.getDistance(aux[i], aux[j]);
-				if(distance < delta){
-					delta = distance;
-					if(distance < this.bestDistance)
-					{
-						this.bestDistance = delta;
-						this.best1 = aux[i];
-						this.best2 = aux[j];
+				if(this.pointsOnSameSegment(aux[i], aux[j]) == false){
+					double distance = GeometryTools2D.getDistance(aux[i], aux[j]);
+					if(distance < delta){
+						delta = distance;
+						if(distance < this.bestDistance)
+						{
+							this.bestDistance = delta;
+							this.best1 = aux[i];
+							this.best2 = aux[j];
+						}
 					}
 				}
 			}
@@ -96,14 +103,30 @@ public class ClosestPair {
 		return delta;
 	}
 	
-	public Point2D either() { return best1; }
-	public Point2D other() { return best2; }
+	public MyPoint2D either() { 
+		if(this.points1.contains(best1))
+		{
+			return best1;
+		}else
+		{
+			return best2;
+		}
+	}
+	public MyPoint2D other() {
+		if(this.points2.contains(best2))
+		{
+			return best2;
+		}else{
+			return best1;
+		}
+		 
+	}
 	
 	public double distance(){
 		return this.bestDistance;
 	}
 	
-	public Boolean pointsOnSameSegment(Point2D p1, Point2D p2)
+	public Boolean pointsOnSameSegment(MyPoint2D p1, MyPoint2D p2)
 	{
 		if(
 				(this.points1.contains(p1) && this.points1.contains(p2))
--- branches/abel/rcr-maslab/src/util/MASLAB/Point2DXSorter.java
+++ branches/abel/rcr-maslab/src/util/MASLAB/Point2DXSorter.java
@@ -1,12 +1,12 @@
 package util.MASLAB;
 
 import java.util.Comparator;
-import rescuecore2.misc.geometry.Point2D;
+import util.MyPoint2D;
 
-public class Point2DXSorter implements Comparator<Point2D> {
+public class Point2DXSorter implements Comparator<MyPoint2D> {
 
 	@Override
-	public int compare(Point2D p1, Point2D p2)
+	public int compare(MyPoint2D p1, MyPoint2D p2)
 	{
 		return (p1.getX() > p2.getX() ? -1 : (p1.getX() == p2.getX() ? 0 : 1));
 	}
