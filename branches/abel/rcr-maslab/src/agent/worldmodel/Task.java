74bdb1aa0ab93bb2cac3bd4558ef16f7cbac8762
phd.abel@gmail.com <phd.abel@gmail.com@150ea5b6-1bd1-6676-5749-7374f3dfea70>
2013-09-13 04:49:01 +0000

    alteraçoes no policial

--- branches/abel/rcr-maslab/src/agent/worldmodel/Task.java
+++ branches/abel/rcr-maslab/src/agent/worldmodel/Task.java
@@ -13,10 +13,20 @@ public class Task implements Serializable{
 	private Object object;
 	private Integer position;
 	private int numberTokens;
+	private int x,y;
 	
 	public Task(Integer id)
 	{
 		this.setId(id);
+	}
+	
+	public Task(Integer id, Object object, Integer x, Integer y)
+	{
+		this.setId(id);
+		this.setObject(object);
+		this.setX(x);
+		this.setY(y);
+		
 	}
 	
 	public Task(Integer id, Object object, Integer position)
@@ -56,6 +66,22 @@ public class Task implements Serializable{
 
 	public void setNumberTokens(int numberTokens) {
 		this.numberTokens = numberTokens;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
 	}
 	
 
